abc5aeca81cc7045f425898fcbb06787ec3c5f3b
Daniel-Huertas764 <ahuertash1@miumg.edu.gt>
2025-02-16 11:52:58 -0600

    se crearon la clase persona y cliente

---
+++ Cliente.cpp
@@ -0,0 +1,28 @@
+#include "Persona.cpp" 
+#include <iostream>
+
+using namespace std;
+
+class Cliente : Persona {
+	private :
+		string nit;
+		
+	// metrodos (constructor y metodos crud)
+	public : 
+		Cliente (){
+			
+		}	
+		Cliente(string nom, string ape, string dir, int tel, string fn, string n) :  Persona(nom, ape, dir, tel, fn){
+			nit = n;
+			
+		}
+		void leer (){
+			cout<<"Nit: "<< nit<<endl;
+			cout<<"Nombres: "<< nombres <<endl;
+			cout<<"Apellidos: "<< apellidos<< endl;
+			cout<<"Direccion: "<< direccion<< endl;
+			cout<<"Telefono: " << telefono << endl;
+			cout<<"Fecha de Nacimiento: "<< fecha_nacimiento << endl; 
+			
+		}
+};
